fb4047fc6bdf8278e8e3894b0bc920a7053486ec
PPPOMA <romaafon22@hotmail.com>
2021-12-15 17:23:06 +0300

    Update task_10.php

--- task_10/task_10.php
+++ task_10/task_10.php
@@ -64,8 +64,8 @@
         </main>
         
 
-        <script src="js/vendors.bundle.js"></script>
-        <script src="js/app.bundle.js"></script>
+        <script src="../js/vendors.bundle.js"></script>
+        <script src="../js/app.bundle.js"></script>
         <script>
             // default list filter
             initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
